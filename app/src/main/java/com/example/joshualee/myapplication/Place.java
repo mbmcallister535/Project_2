de03f224c6915cd5b7569375f614d6aed60ab590
Joshua Lee <joshuamorganlee@gmail.com>
2016-10-12 10:52:54 -0500

    First commit

---
+++ app/src/main/java/com/example/joshualee/myapplication/Place.java
@@ -0,0 +1,16 @@
+package com.example.joshualee.myapplication;
+
+/**
+ * Created by joshualee on 10/12/16.
+ */
+public class Place {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
