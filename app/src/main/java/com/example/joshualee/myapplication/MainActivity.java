8fb119d3da64f8012a1a14146abd5fcb41a316cf
Fariz Rahmat <kamarulfariz.rahmat@drake.edu>
2016-10-23 15:28:11 -0500

    change location of menu and logo

--- app/src/main/java/com/example/joshualee/myapplication/MainActivity.java
+++ app/src/main/java/com/example/joshualee/myapplication/MainActivity.java
@@ -4,13 +4,21 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import static android.R.id.button1;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,29 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         //getSupportActionBar().setIcon(R.drawable.mycornerlogo_01_24px_2);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        button1 = (ImageButton) findViewById(R.id.menu_button);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Creating the instance of PopupMenu
+                PopupMenu popup = new PopupMenu(MainActivity.this, button1);
+                //Inflating the Popup using xml file
+                popup.getMenuInflater().inflate(R.menu.menu_filter, popup.getMenu());
+
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MainActivity.this,"You Clicked : " + item.getTitle(),Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+
+                popup.show();//showing popup menu
+            }
+        });//closing the setOnClickListener method
+
 
         final int[] icons = new int[] {
             R.drawable.ic_home_black_24px,
@@ -56,10 +87,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
+    // menu button using app bar support design
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_filter, menu);
         return true;
-    }
+    }*/
 }
