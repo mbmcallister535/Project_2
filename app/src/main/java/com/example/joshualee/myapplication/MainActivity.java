8695ddd7b19a7be63b128f7b578876e199818ad4
mbmcallister <michael.mcallister@drake.edu>
2016-10-26 16:47:38 -0500

    Location Services figure out

--- app/src/main/java/com/example/joshualee/myapplication/MainActivity.java
+++ app/src/main/java/com/example/joshualee/myapplication/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.joshualee.myapplication;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -14,8 +20,15 @@ import android.app.ListActivity;
 import android.app.ActivityOptions;
 import android.view.View;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private GoogleApiClient mGoogleApiClient;
+    private double user_latitude;
+    private double user_longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +37,28 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final int[] icons = new int[] {
-            R.drawable.ic_home_black_24px,
-            R.drawable.ic_replay_black_24px,
-            R.drawable.ic_favorite_black_24px
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        final int[] icons = new int[]{
+                R.drawable.ic_home_black_24px,
+                R.drawable.ic_replay_black_24px,
+                R.drawable.ic_favorite_black_24px
         };
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        for (int k=0; k < icons.length; k++) {
+        for (int k = 0; k < icons.length; k++) {
             tabLayout.addTab(tabLayout.newTab().setIcon(icons[k]));
         }
         /*tabLayout.addTab(tabLayout.newTab().setText("Home"));
         tabLayout.addTab(tabLayout.newTab().setText("Recent"));
         tabLayout.addTab(tabLayout.newTab().setText("Favorite"));
-        */tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        */
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         final PagerAdapter adapter = new PagerAdapter
@@ -61,6 +82,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.v("hi","onConnected");
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            user_latitude = mLastLocation.getLatitude();
+            user_longitude = mLastLocation.getLongitude();
+
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.v("hi","onConnectionFailed");
+    }
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     /*@Override
